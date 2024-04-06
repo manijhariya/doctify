70173e2a70a39e998f2952bebd179c614b220152
Manish Kumar <mannjhariya@gmail.com>
2024-04-07 03:27:08 +0530

    fixed inference file

--- src/inference.py
+++ src/inference.py
@@ -1,43 +1,56 @@
-# %%
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from prompts import default_prompt
 
 torch.set_default_device("cuda")
 torch.cuda.empty_cache()
 
 
-model_path = "./models/phi2/checkpoint-100"
-# model_path = "microsoft/phi-2"
+class Inference:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
 
-model = AutoModelForCausalLM.from_pretrained(
-    model_path,
-    torch_dtype=torch.float16,
-    device_map={"": 0},
-)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-# tokenizer.pad_token_id = tokenizer.eos_token_id
-# tokenizer.padding_side = "right"
-# %%
-# in_text = "I have sucidial thoughts what should we do?"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
 
-in_text = """<|beginoftext|>def get_crypto_input(self, kt_element: NamedTuple) -> AbstractCryptoInput:
-    return CryptoInput(kt_element=kt_element)<|separateoftext|>"""
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_name,
+            torch_dtype=torch.float16,
+            device_map={"": 0},
+        )
 
-code = """def __clean_code__(self, source_code : str, docstring : str) -> Tuple[str, str]:
-    if not docstring:
-        return None, None
-    source_code = source_code.replace(docstring, "")
-    docstring = docstring.replace('"', "")
-    docstring = docstring.replace("'", "")
-    return source_code, docstring #re.sub('\s+', ' ', source_code)"""
+    def post_process_text(self, output_text: str) -> str:
+        return (
+            output_text.split("<|separateoftext|>")[-1]
+            .replace("<|endoftext|>", "")
+            .strip()
+        )
+
+    def generate_docstring(
+        self, code: str, language: str = "python", max_length: int = 200
+    ) -> str:
+        prompt = default_prompt.format(code=code)
+        inputs = self.tokenizer(
+            prompt, return_tensors="pt", return_attention_mask=False
+        )
+
+        outputs = self.model.generate(**inputs, max_length=max_length)
+
+        return self.post_process_text(self.tokenizer.batch_decode(outputs)[0])
 
 
-in_text = f"<|beginoftext|>{code}<|separateoftext|>"
-inputs = tokenizer(in_text, return_tensors="pt", return_attention_mask=False)
+if __name__ == "__main__":
+    model_name = "./models/phi2-phase2/checkpoint-600"
 
-outputs = model.generate(
-    **inputs,
-    max_length=200,
-)
-text = tokenizer.batch_decode(outputs)[0]
-print(text)
+    code = """def __clean_code__(self, source_code : str, docstring : str) -> Tuple[str, str]:
+        if not docstring:
+            return None, None
+        source_code = source_code.replace(docstring, "")
+        docstring = docstring.replace('"', "")
+        docstring = docstring.replace("'", "")
+        return source_code, docstring #re.sub('\s+', ' ', source_code)"""
+
+    inference = Inference(model_name)
+    docstring = inference.generate_docstring(code, max_length=400)
+
+    print(docstring)
